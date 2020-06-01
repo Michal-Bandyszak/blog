f7aa3bae89419d4af003b4bb82489e9484a5fbb6
michal bandyszak <michalbandyszak@gmail.com>
2020-06-01 14:35:39 +0200

    working version

--- users.js
+++ users.js
@@ -2,6 +2,7 @@ import { handleError } from './modules/handleError.js';
 import { userComponent } from './modules/userComponent.js';
 
 const btnAddUserModal = document.getElementById("btnAddUserModal");
+const tbody = document.getElementById("usersTable__body")
 
 const getAllUsers = () => 
   fetch("http://localhost:8090/v1/users/get")
@@ -11,15 +12,41 @@ const getAllUsers = () =>
     }).catch(handleError)
   
 
-const addUser = () => 
+const addUser = (user) => 
   fetch("http://localhost:8090/v1/users/add", {
     method: "POST",
     headers:  {
       "Content-Type": "application"
-    }
-  }).catch(handleError)
+    },
+    body: JSON.stringify(user),
+	}).then(res => res.json())
+		.catch(handleError);
 
-getAllUsers()
+  const showUsers = () => {
+    tbody.innerHTML = "";
+    getAllUsers()
+  }
+
+  showUsers()
+
+  document.forms.formAdd.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const body = Object.fromEntries(new FormData(e.target));
+  
+    const data = Object.entries(body).reduce((prev, actual) => {
+      const [key, value] = actual;
+      if(key === 'gender') {
+        return { ...prev, [key]: +value }
+      };
+      
+      return { ...prev, [key]: value };
+      
+    }, {});
+     addUser(data).then(showUsers)
+     $('#newUser').modal('hide').find('textarea,input').val('');
+  })
+
+  
 
 btnAddUserModal.addEventListener("click", () => {
   $('#newUser').modal('show').find('textarea,input').val('');
